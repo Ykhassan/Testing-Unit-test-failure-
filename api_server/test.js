d8ced0f60f365005dbb85102e76b9fc4cba60b3b
Yhassan <156916988+Ykhassan@users.noreply.github.com>
2025-01-27 04:54:45 +0300

    Update test.js

--- api_server/test.js
+++ api_server/test.js
@@ -1,1 +1,15 @@
 console.log("Inside container")
+
+ let x = await fetch('http://localhost:4000/api/health', {
+    method: 'GET', // you can explicitly specify the method if you want, it's optional for GET requests
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer cdhub-api-key-123'
+    }
+  });
+
+  x = await x.json();
+  console.log(x);
+}
+
+hello();
