38ee1125f916ff4ba0dded6b1df59b0f543da365
Yhassan <156916988+Ykhassan@users.noreply.github.com>
2025-01-27 04:57:36 +0300

    Update test.js

--- api_server/test.js
+++ api_server/test.js
@@ -1,6 +1,7 @@
 console.log("Inside container")
 
- let x = await fetch('http://localhost:4000/api/health', {
+async function hello() { 
+let x = await fetch('http://localhost:4000/api/health', {
     method: 'GET', // you can explicitly specify the method if you want, it's optional for GET requests
     headers: {
       'Accept': 'application/json',
